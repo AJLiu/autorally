4c9d47f14c70bf81ed1407d8ad45ffd098ef4080
Anthony Liu <ajliu@gatech.edu>
2019-02-18 11:52:41 -0500

    prevent spawning on obstacles and add image collection

--- autorally_gazebo/nodes/autorally_random_pose.py
+++ autorally_gazebo/nodes/autorally_random_pose.py
@@ -11,10 +11,13 @@ from tf.transformations import quaternion_from_euler
 import time
 import random
 import math
+import numpy as np
+import ros_numpy
+from sensor_msgs.msg import Image
 
 
 def setup_log(log_name):
-
+    """Initializes handlers for logging"""
     logger = logging.getLogger(log_name)
     logger.setLevel(logging.DEBUG)
     # fh = logging.FileHandler(path + log_name)
@@ -29,6 +32,7 @@ def setup_log(log_name):
     return logger
 
 def set_pose(pose, x, y, yaw):
+    """Converts yaw into quaternion coordinates and assigns the values to a pose object"""
     pose.position.x = x
     pose.position.y = y
     pose.position.z = 0.3
@@ -38,37 +42,101 @@ def set_pose(pose, x, y, yaw):
     pose.orientation.z = qz
     pose.orientation.w = qw
 
+def is_free(x, y, size):
+    """Return True if the square of length 'size' centered at (x,y) does not 
+    contain an obstacle"""
+
+    if x is None or y is None:
+        return False
+
+    _x = int((x + 15) * ppm)
+    _y = int((y + 15) * ppm)
+    _size = int(size * ppm / 2.0)
+
+    free = np.all(cost_map[_y-_size : _y+_size, _x-_size : _x+_size] < 1)
+    return free
+
+def image_left_callback(msg):
+    global image_left
+    """Process images from the left camera."""
+    image_left = ros_numpy.numpify(msg)
+
+def image_right_callback(msg):
+    global image_right
+    """Process images from the right camera."""
+    image_right = ros_numpy.numpify(msg)
+
+
 def main():
     get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
     set_state = rospy.ServiceProxy("/gazebo/set_model_state",SetModelState)
 
+    data = []
+
+    # Intialize msg objects
     pose = Pose()
-
-
     modelstate = ModelState()
     modelstate.model_name = "Alpha"
 
-    for i in range(num_runs):
-        logger.info(i)
+    # Wait for images before starting
+    time.sleep(1)
 
-        x = random.uniform(-10, 10)
-        y = random.uniform(-10, 10)
+    for i in range(num_runs):
+        x,y = None, None
         yaw = random.uniform(0, 2*math.pi)
+
+        # Spawn in a random free position in the track
+        while not is_free(x,y,1):           
+            x = random.uniform(-15, 15)
+            y = random.uniform(-15, 15)
+
+        logger.info('{}: global({: >6.2f},{: >6.2f}) : {}'.format( \
+                    i, x, y, is_free(x, y, 1)))
+
 
         set_pose(pose, x, y, yaw)
         modelstate.pose = pose
         set_state(modelstate)
 
-        logger.info(get_state('Alpha', 'world'))
-        time.sleep(5)
+        # We spawn the car slightly off the ground, so this lets it fall into place
+        time.sleep(.1)
+
+        # Save current image
+        data.append([image_left, image_right])
+        time.sleep(.1)
+
+
 if __name__ == '__main__':
     name = 'random_pose'
 
     rospy.init_node(name)
     
-    num_runs = rospy.get_param('~num_runs')
+    num_runs = rospy.get_param('~num_runs', 10)
+    map_path = rospy.get_param('~map_path')
+    x_origin = rospy.get_param('~x_origin')
+    y_origin = rospy.get_param('~y_origin')
+    image_left_topic = rospy.get_param('~image_left_topic', '/left_camera/image_raw')
+    image_right_topic = rospy.get_param('~image_right_topic', '/right_camera/image_raw')
+    _cost_map = np.load(map_path)
 
+    # Configure cost map
+    x_min, x_max = _cost_map['xBounds']
+    y_min, y_max = _cost_map['yBounds']
+    ppm = _cost_map['pixelsPerMeter']
+    vals = _cost_map['channel0']
+
+    cost_map = vals.reshape(int((y_max-y_min) * ppm), int((x_max-x_min) * ppm))
+
+    # Configure images
+    image_left = None
+    image_right = None
+    image_left_sub = rospy.Subscriber(image_left_topic, Image, image_left_callback)
+    image_right_sub = rospy.Subscriber(image_right_topic, Image, image_right_callback)
+
+    # Configure logger
     logger = setup_log(name)
     logger = logging.getLogger(name)
+
+    # Run script
     main()
     # rospy.spin()
